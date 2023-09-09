4d26853b4a60fbf74729d7d6ccfaf4cfa78798d0
johnny <johnny.goulb@gmail.com>
2023-09-09 14:52:25 -0400

    new backend homepage; file structure changes; db+

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,22 @@
  * Initialize required modules for API, then launch to listen for requests.
  */
 
+require('dotenv/config')
 const express = require('express')
+const path = require('path')
 const favicon = require('serve-favicon')
+
+
 const app = express()
-const port = 3333
+const port = process.env.PORT
 
-app.use(favicon('./favicon.ico'));
-
+app.use(favicon('./favicon.ico'))
+app.use(express.static('src/public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, './index.html'))
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on Port ${port}. Enjoy! 🤍`)
 })
---
+++ backend/src/db/LIFELeagueGames_2023-09-09.sql
@@ -0,0 +1,57 @@
+# ************************************************************
+# Sequel Ace SQL dump
+# Version 20051
+#
+# https://sequel-ace.com/
+# https://github.com/Sequel-Ace/Sequel-Ace
+#
+# Host: 127.0.0.1 (MySQL 8.1.0)
+# Database: LIFELeagueGames
+# Generation Time: 2023-09-09 18:48:50 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE='NO_AUTO_VALUE_ON_ZERO', SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table Admin
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Admin`;
+
+CREATE TABLE `Admin` (
+  `adminID` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` char(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` char(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`adminID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+# Dump of table Player
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `Player`;
+
+CREATE TABLE `Player` (
+  `playerID` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` char(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` char(255) NOT NULL,
+  PRIMARY KEY (`playerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
