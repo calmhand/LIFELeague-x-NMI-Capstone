75f16e289cb8714bd27136b22accbce477bfb872
johnny <johnny.goulb@gmail.com>
2023-09-23 23:38:23 -0400

    +sounds to nav bar

--- frontend/components/global/NavBar.vue
+++ frontend/components/global/NavBar.vue
@@ -1,16 +1,17 @@
 <template>
     <nav class="nav-bar">
-        <button class="nav-link" @click="goTo('/home')" id="home-btn">HOME</button>
-        <button class="nav-link" @click="goTo(`/${store.getUsername}/skills`)" id="skills-btn">SKILLS</button>
-        <button class="nav-link" @click="goTo(`/${store.getUsername}/stats`)"  id="stats-btn">STATS</button>
-        <button class="nav-link" @click="goTo(`/account/player-${store.getUsername}`)"  id="settings-btn">SETTINGS</button>
-        <button class="nav-link" id="logout-btn" @click="logout"><img src="@/assets/images/logoutIcon.png" alt="Logout"/></button>
+        <button class="nav-link" @mouseover="playSound()" @click="goTo('/home')" id="home-btn">HOME</button>
+        <button class="nav-link" @mouseover="playSound()" @click="goTo(`/${store.getUsername}/skills`)" id="skills-btn">SKILLS</button>
+        <button class="nav-link" @mouseover="playSound()" @click="goTo(`/${store.getUsername}/stats`)"  id="stats-btn">STATS</button>
+        <button class="nav-link" @mouseover="playSound()" @click="goTo(`/account/player-${store.getUsername}`)"  id="settings-btn">SETTINGS</button>
+        <button class="nav-link" @mouseover="playSound()" id="logout-btn" @click="logout"><img src="@/assets/images/logoutIcon.png" alt="Logout"/></button>
     </nav>
 </template>
 <script>
 // import { useAuthStore } from '@/stores/auth'; // import the auth store we just created
 import { useTestStore } from '@/stores/temp.auth'; // import the auth store we just created
-
+import select_one from '@/assets/sounds/select.mp3'
+import select_two from '@/assets/sounds/select_2.mp3'
 export default {
     setup() {
         const { logUserOut } = useTestStore(); // use authenticateUser action from  auth store
@@ -32,7 +33,17 @@ export default {
             return navigateTo(`/account/player-${this.authStore.getUsername}`, {external: true})
         },
         goTo(url) {
+            const selectSoundTwo = new Audio(select_two)
+            selectSoundTwo.volume = 0.3
+            selectSoundTwo.play()
             navigateTo(url)
+        },
+        playSound() {
+            const selectSoundOne = new Audio(select_one)
+            selectSoundOne.volume = 0.3
+            selectSoundOne.pause()
+            selectSoundOne.currentTime = 0
+            selectSoundOne.play()
         },
         setupBg() {
             // document.getElementById("video-bg").style.display = "none"
@@ -60,6 +71,7 @@ export default {
     },
     mounted() {
         // this.setupBg() //pain
+        // this.setUpSounds()
     }
 }
 </script>
