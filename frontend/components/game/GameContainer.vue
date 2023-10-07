712472dd9ea4aaa0b2ce51042fcda56c112edacc
johnny <johnny.goulb@gmail.com>
2023-10-07 17:13:14 -0400

    +phaser successfully embedded into nuxt

---
+++ frontend/components/game/GameContainer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="game">
+        <button id="start-btn" v-if="showBtn" @click="startGame()">start game</button>
+    </div>
+</template>
+
+<script>
+import { launch } from "@/game/game"
+export default {
+    name: "GameContainer",
+    data() {
+        return {
+            showBtn: true
+        }
+    },
+    methods: {
+        startGame() {
+            this.showBtn = false
+            launch()
+        }
+    }
+}
+</script>
+<style lang="scss">
+    #game {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
--- frontend/pages/game/index.vue
+++ frontend/pages/game/index.vue
@@ -7,7 +7,7 @@
             </div>
         </div>
         <div id="stage-select">
-            <button id="stage-one-btn" @mouseover="setContent(stageOne)" class="stage-btn">STAGE ONE</button>
+            <button id="stage-one-btn" @mouseover="setContent(stageOne)" @click="navigateTo('/game/stage1')" class="stage-btn">STAGE ONE</button>
             <button id="stage-two-btn" v-if="stageTwo.available" @mouseover="setContent(stageTwo)" class="stage-btn">STAGE TWO</button>
             <button id="stage-three-btn" v-if="stageThree.available" @mouseover="setContent(stageThree)" class="stage-btn">STAGE THREE</button>
             <button id="stage-one-btn" v-if="!stageThree.available" class="stage-btn">LOCKED</button>
